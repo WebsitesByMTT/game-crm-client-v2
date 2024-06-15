5ec687d87cd3870fda4357dc533e4e58bedee41c
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-06-15 04:49:11 -0700

    change

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '25mb',
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
+
+
