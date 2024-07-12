4175e907b92bcecb77b1af1608c7d15c0de0cf7d
Ashish Soni <ashishsoni.underpinservices@gmail.com>
2024-07-12 05:04:59 -0700

    padding in b/w platform cards.

--- src/app/(home)/game/page.js
+++ src/app/(home)/game/page.js
@@ -6,7 +6,7 @@ const page = async () => {
   const platforms = await getPlatform()
   return (
     <div>
-      <div className="grid gap-5 grid-cols-12 pt-10 pl-1 md:pl-10">
+      <div className="grid gap-5 grid-cols-12 pt-10 pl-1 md:px-10">
         {
           platforms?.map((item, ind) => (
             <Link href={`/game/${item.name}`} key={ind} className="rounded-[.3rem] capitalize tracking-wide text-base text-center py-6 bg-white dark:bg-Dark_light text-black dark:text-white col-span-12 md:col-span-6 lg:col-span-3 shadow-xl cursor-pointer hover:scale-95 border dark:border-gray-600 transition-all dark:shadow-Dark">
